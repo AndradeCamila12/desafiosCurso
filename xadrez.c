be66afbc2daafc8541746b7c76176e6a05db35cc
AndradeCamila12 <camiladecastro94@gmail.com>
2025-05-24 17:29:29 +0000

    Desafio xadrez - nível AVENTUREIRO

--- xadrez.c
+++ xadrez.c
@@ -8,12 +8,17 @@ int main(){
 
 int torre = 1;
 
-printf("Movimentando a torre:\n");
-while ((torre <= 5) && (torre != 0))
-{
-    printf("direita\n");
-    torre++;
-}
+    printf("Movimentando a torre:\n");
+    while ((torre <= 5) && (torre != 0))
+    {
+       printf("direita\n");
+       torre++;
+    }
+
+printf("\n");
+printf(" *** --------------------------- ***\n");
+printf("\n");
+
 
 //movendo o BISPO 5 casas na diagonal cima-direita utilizando DO-WHILE:
 printf("Movimentando o bispo:\n");
@@ -40,13 +45,47 @@ do {
         }
     } while (bispo < 5);
 
+printf("\n");
+printf(" *** --------------------------- ***\n");
+printf("\n");
+
+
 //movendo a RAINHA 8 casas para a esquerda usando FOR:
 
-    for (int rainha = 0; rainha < 8; rainha++)
+printf("Movimentando a rainha:\n");
+
+    for (int rainha = 0; rainha < 9; rainha++)
     {
         printf("esquerda\n");
     }
     
+printf("\n");
+printf(" *** --------------------------- ***\n");
+printf("\n");
+
+////movendo o CAVALO 2 casas para a esquerda e 1 para a esquerda:
+
+printf("Movimentando o cavalo:\n");
+
+int i = 0;
+
+    while (i < 2) {
+        printf("baixo\n");
+
+		if (i == 1) {
+            for (int j = 0; j < 1; j++) {
+                printf("esquerda\n");
+            }
+        }
+
+        i++;
+    }
+    
+printf("\n");
+printf(" *** --------------------------- ***\n");
+printf("\n");
+
+
 
     return 0;
 }
